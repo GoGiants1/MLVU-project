1c6a5d243536343835c1fcc3d4f2a90f67877d94
GoGiants1 <chooi221@snu.ac.kr>
2024-05-11 15:59:23 +0900

    Add Cross frame attention using torch 2

--- style_transfer/pipelines/inverted_ve_pipeline.py
+++ style_transfer/pipelines/inverted_ve_pipeline.py
@@ -564,6 +564,86 @@ class CrossFrameAttnProcessor:
         return hidden_states
 
 
+class CrossFrameAttnProcessor2_0:
+    def __init__(
+        self, unet_chunk_size=2, attn_map_save_steps=[], activate_step_indices=None
+    ):
+        self.unet_chunk_size = unet_chunk_size
+        self.attn_map_save_steps = attn_map_save_steps
+        self.activate_step_indices = activate_step_indices
+
+    def __call__(
+        self, attn, hidden_states, encoder_hidden_states=None, attention_mask=None
+    ):
+
+        if not hasattr(attn, "attn_map"):
+            setattr(attn, "attn_map", {})
+            setattr(attn, "inference_step", 0)
+        else:
+            attn.inference_step += 1
+
+        batch_size, sequence_length, _ = hidden_states.shape
+        attention_mask = attn.prepare_attention_mask(
+            attention_mask, sequence_length, batch_size
+        )
+        query = attn.to_q(hidden_states)
+
+        is_cross_attention = encoder_hidden_states is not None
+        if encoder_hidden_states is None:
+            encoder_hidden_states = hidden_states
+
+        key = attn.to_k(encoder_hidden_states)
+        value = attn.to_v(encoder_hidden_states)
+
+        is_in_inference_step = False
+
+        if self.activate_step_indices is not None:
+            for activate_step_index in self.activate_step_indices:
+                if (
+                    attn.inference_step >= activate_step_index[0]
+                    and attn.inference_step <= activate_step_index[1]
+                ):
+                    is_in_inference_step = True
+                    break
+
+        # Swapping Attention
+        if not is_cross_attention and is_in_inference_step:
+            key, value = swapping_attention(key, value, self.unet_chunk_size)
+
+        query = attn.head_to_batch_dim(query)
+        key = attn.head_to_batch_dim(key)
+        value = attn.head_to_batch_dim(value)
+
+        attention_probs = attn.get_attention_scores(query, key, attention_mask)
+
+        if attn.inference_step in self.attn_map_save_steps:
+            attn.attn_map[attn.inference_step] = attention_probs.clone().cpu().detach()
+
+        inner_dim = hidden_states.shape[-1]
+        head_dim = inner_dim // attn.heads
+
+        # the output of sdp = (batch, num_heads, seq_len, head_dim)
+        # TODO: add support for attn.scale when we move to Torch 2.1
+        hidden_states = nnf.scaled_dot_product_attention(
+            query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
+        )
+
+        hidden_states = hidden_states.transpose(1, 2).reshape(
+            batch_size, -1, attn.heads * head_dim
+        )
+        hidden_states = hidden_states.to(query.dtype)
+
+        # linear proj
+        hidden_states = attn.to_out[0](hidden_states)
+        # dropout
+        hidden_states = attn.to_out[1](hidden_states)
+
+        if attn.inference_step == 49:
+            attn.inference_step = -1
+
+        return hidden_states
+
+
 class CrossFrameAttnProcessor4Inversion:
     def __init__(
         self, unet_chunk_size=2, attn_map_save_steps=[], activate_step_indices=None
