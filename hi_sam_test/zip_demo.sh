751736018238461fc51979767acf4b0df0d9e874
ksyint <82583462+ksyint@users.noreply.github.com>
2024-05-11 21:14:43 +0900

    Delete hi_sam_test directory

--- hi_sam_test/zip_demo.sh
+++
@@ -1,5 +0,0 @@
-zip -r demo_line_detect_w_zeroshot demo_line_detect_w_zeroshot/
-zip -r demo_line_detect_wo_zeroshot demo_line_detect_wo_zeroshot/
-zip -r demo_word_detect_wo_zeroshot demo_word_detect_wo_zeroshot/
-zip -r demo_word_detect_w_zeroshot demo/
-
