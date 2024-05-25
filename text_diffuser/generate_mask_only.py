8a2605f9b3a70047cc470c72a315e5516474a786
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2024-05-25 18:13:33 +0900

    Research/attn proc (#16)
    * Add attn vis util
    
    * Init Attention processors
    
    * Update
    
    * Update hook_fn
    
    * Add attn map in proc
    
    * misc
    
    * Update demo
    
    * misc
    
    * Add img2img pipeline
    
    * init notebook
    
    * Remove for loop
    
    * Add demo for img2img

--- text_diffuser/generate_mask_only.py
+++ text_diffuser/generate_mask_only.py
@@ -24,7 +24,7 @@ def gen_mask_only(image, sample_text, choice_list, arg_textseg, arg_maskgen):
 
     """""
 
-    choice_list 는 scene text 가 가장 위 나 왼쪽에 있는것을 choice_list의 첫번째요소로간주한다. 이때 choice_list의 첫요소가 1이면 
+    choice_list 는 scene text 가 가장 위 나 왼쪽에 있는것을 choice_list의 첫번째 요소로 간주한다. 이때 choice_list의 첫요소가 1이면 
     scene text 중 가장 왼쪽, 가장 위인것만 output에서 sample text가 나타난다.
     나머지 choice list에서 0인것들은 text stroke 된것이 나타난다
 
@@ -36,20 +36,24 @@ def gen_mask_only(image, sample_text, choice_list, arg_textseg, arg_maskgen):
     masked_text_stroke = run_text_stroke_segmentation(sam_detector=sam, image=image, patch_mode=True)
 
     mask3=np.zeros_like(masked_text_stroke)
-    for w in range(masked_text_stroke.shape[0]):
-        for h in range(masked_text_stroke.shape[1]):
-            if masked_text_stroke[w,h]==True:
-                mask3[w,h]=0.0
-            else:
-                mask3[w,h]=255.0
+
+    # tss = Image.fromarray(masked_text_stroke)
+    # tss.save("tss.png")
+    mask3 = np.where(masked_text_stroke > 0, 0.0, 255.0)
+
+    # mask3_image = Image.fromarray(mask3, "L")
+    # save mask3
+    # mask3_image.save("mask3.png")
 
     stroke_mask=mask3
     #검정색 글씨 하얀배경
     masks=mask.squeeze(1)
+
     new_mask=np.zeros_like(masks[0,:,:])
 
     for mask in masks:
         new_mask+=mask
+
 
     new_mask=Image.fromarray(new_mask)
     new_mask2=new_mask
